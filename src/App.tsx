15dcae332b79425c3b3bf3c7105fed9555f9c791
Arkadina <61208486+Arkadina@users.noreply.github.com>
2023-03-18 00:18:12 -0300

    props type + setSelected

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import Dispatch, { useState, SetStateAction } from "react";
 import { ArrowDownIcon } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 
@@ -13,20 +13,26 @@ type TStates = {
 
 function App() {
     const [isDropdownActive, setIsDropdownActive] = useState(false);
+    const [selected, setSelected] = useState<TStates[]>([] as TStates[]);
 
+    console.log(selected);
     return (
         <div className="App">
             <button
                 className="buttonState"
                 onClick={() => setIsDropdownActive(!isDropdownActive)}
             >
-                Select state
+                {selected.length} Select state
                 <ArrowDownIcon className="icon h-1 w-1 text-blue-500" />
             </button>
             {isDropdownActive && (
                 <div className="stateContainer">
                     {states.map((item) => (
-                        <GridItemContainer item={item} />
+                        <GridItemContainer
+                            item={item}
+                            setSelected={setSelected}
+                            selected={selected}
+                        />
                     ))}
                 </div>
             )}
@@ -36,10 +42,29 @@ function App() {
 
 export default App;
 
-function GridItemContainer({ item }: { item: TStates }) {
+function GridItemContainer({
+    item,
+    setSelected,
+    selected,
+}: {
+    item: TStates;
+    setSelected: Function;
+    selected: TStates[];
+}) {
+    const [isChecked, setIsChecked] = useState(false);
+
+    function handleOnChange(item: TStates) {
+        setIsChecked(!isChecked);
+        setSelected([...selected, item]);
+    }
+
     return (
         <div>
-            <input type="checkbox" />
+            <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={(e) => handleOnChange(item)}
+            />
             <span>{item.name}</span>
         </div>
     );
